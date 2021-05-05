9038f658c91e22dcd12dac9c5eedb1731b80ec75
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-05-06 01:34:54 +0530

    user authentication with firebase - only sigin

---
+++ Firebase/UserAuthentication with firebase/DataClusterPrototype/app/src/main/java/com/pracheejaviya/dataclusterprototype/views/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.pracheejaviya.dataclusterprototype.views
+
+import com.pracheejaviya.dataclusterprototype.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ Firebase/UserAuthentication with firebase/DataClusterPrototype/app/src/main/java/com/pracheejaviya/dataclusterprototype/views/SigninActivity.kt
@@ -0,0 +1,54 @@
+package com.pracheejaviya.dataclusterprototype.views
+
+import android.content.Intent
+import com.pracheejaviya.dataclusterprototype.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import com.pracheejaviya.dataclusterprototype.extensions.Extensions.toast
+import com.pracheejaviya.dataclusterprototype.utils.FirebaseUtils.firebaseAuth
+import kotlinx.android.synthetic.main.activity_signin.*
+
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+        btnSignIn.setOnClickListener {
+            signInUser()
+        }
+    }
+    lateinit var signInEmail: String
+    lateinit var signInPassword: String
+    lateinit var signInInputsArray: Array<EditText>
+
+    private fun notEmpty(): Boolean = signInEmail.isNotEmpty() && signInPassword.isNotEmpty()
+
+    private fun signInUser() {
+        signInEmail = etSignInEmail.text.toString().trim()
+        signInPassword = etSignInPassword.text.toString().trim()
+
+        if (notEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(signInEmail, signInPassword)
+                .addOnCompleteListener { signIn ->
+                    if (signIn.isSuccessful) {
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        toast("signed in successfully")
+                        finish()
+                    } else {
+                        toast("sign in failed")
+                    }
+                }
+        } else {
+            signInInputsArray.forEach { input ->
+                if (input.text.toString().trim().isEmpty()) {
+                    input.error = "${input.hint} is required"
+                }
+            }
+        }
+    }
+}
+
+
+
+
